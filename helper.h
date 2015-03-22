63c5b81a8498c7edd6f0adde2f0036bc2081ef68
TheKK <thumbd03803@gmail.com>
2015-03-22 10:26:58 +0800

    Separate useful macros to helper.h

---
+++ helper.h
@@ -0,0 +1,11 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#ifndef M_PI
+#define M_PI    3.1415926535
+#endif /* M_PI */
+
+#define DEGREE2ARC(DEGREE) ((DEGREE) * M_PI / 180.0)
+#define ARC2DEGREE(ARC) ((ARC) * 180.0 / M_PI)
+
+#endif /* HELPER_H */
