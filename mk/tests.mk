fcd86c8a1c52e0c9c54d67feed9d6d483146d862
Ying-Ruei Liang (KK) <thumbd03803@gmail.com>
2015-04-22 01:02:36 +0800

    Change variable name in test.mk

--- mk/tests.mk
+++ mk/tests.mk
@@ -4,12 +4,15 @@ TEST_SRCS := $(wildcard $(TEST_SRC_DIR)/*.cpp)
 TEST_OBJS := $(addprefix $(BUILD_DIR)/, $(TEST_SRCS:.cpp=.o))
 TEST_DEPS := $(OBJS:.o=.d)
 
-TEST_EXE := $(BUILD_DIR)/$(BIN_DIR)/testRunner
+TEST_RUNNER := $(BUILD_DIR)/$(BIN_DIR)/testRunner
 
-.PHONY: test
-test: $(TEST_EXE)
+test: $(TEST_RUNNER)
 
 build/bin/testRunner: $(TEST_OBJS) $(filter-out %main.o, $(OBJS))
 	@mkdir -p $(dir $@)
 	@$(CXX) $^ $(LIBS) -o $@
 	@echo "     LD     " $(notdir $@)
+
+.PHONY: runTest
+runTest: $(TEST_RUNNER)
+	@$<
